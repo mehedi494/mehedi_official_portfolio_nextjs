3d0a4e04fefb07b0079a1b3536beabacc076bcbe
mehedi494 <ehasanmunna21@gmail.com>
2022-11-18 01:53:20 +0600

    adde nav bar

---
+++ components/HeroSection.js
@@ -0,0 +1,16 @@
+function HeroSection() {
+    return (<>
+        <div class="mt-5 grid grid-cols-2 gap-4">
+            <div class="...">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, error.
+            </div>
+            <div class="...">
+                <Image></Image>
+            </div>
+                
+                     
+        </div>
+    </>);
+}
+
+export default HeroSection;
--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+
+module.exports = {
+  images: {
+    domains: ['i.ibb.co'],
+  },
+}
