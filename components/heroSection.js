a2d452482ef57c8f6474db78fc7df940ee723c0f
mehedi hasan <ehasanmunna84@gmail.com>
2023-10-28 13:32:16 +0600

    add home page blur backgournd image

--- components/heroSection.js
+++ components/heroSection.js
@@ -8,11 +8,7 @@ function HeroSection({ StyleHeroSec }) {
     useDownloader();
   const fileUrl = "/Full Stack Resume (Oct-2023).pdf";
   const filename = "Full Stack Resume (Oct-2023).pdf";
-  const mernDeveloper = (
-    <p className="font-poppins font-semibold text-4xl sm:text-base">
-      MERN STACK DEVELOPER
-    </p>
-  );
+  
 
   return (
     <>
@@ -21,27 +17,27 @@ function HeroSection({ StyleHeroSec }) {
         sm:grid-col-1  gap-5 w-full  h-full ">
         <div className="lg:order-first md:order first sm:order-last flex-row-1 mx-auto my-auto items-center text-justify p-4">
           <p className="font-poppins font-regular text-base ">
-            Hello, MY NAME IS{" "}
+            Hello 👋 , MY NAME IS
           </p>
 
           <h1 className="text-gray-700 sm:sm:text-2xl md:md:text-3xl text-5xl lg:lg:text-4xl font-Josefin  font-bold dark:text-gray-400  ">
             Mehedi Hasan Munna
           </h1>
-          <div>
-            <p className="inline font-poppins font-semibold text-rose-700 dark:text-orange-300 text-8xl sm:text-base">
+          <div >
+            <h1 className=" font-poppins font-semibold text-rose-700 dark:text-orange-300 md:text-2xl sm:text-base">
               <Typewriter
                 options={{
                   strings: [
                     "MERN STACK DEVELOPER",
                     "FULL STACK DEVELOPER",
-                    "NEXTJS DEVELOPER",
-                    "REACT-NATIVE (Learning)",
+                    "NEXT.JS DEVELOPER",
+                    "MOBILE APP DEVELOPER  (REACT NATIVE Learning)",
                   ],
                   autoStart: true,
                   loop: true,
                 }}
               />
-            </p>
+            </h1>
           </div>
           {/* <p className="font-poppins font-semibold text-2xl sm:text-base">
             Web Developer (MERN STACK)
@@ -53,7 +49,7 @@ function HeroSection({ StyleHeroSec }) {
             work with an end goal in mind.
           </p>
           <div className="mt-5 flex justify-base gap-4">
-            <button className="btn btn-lg no-animation outline-dashed   dark:hover:text-white">
+            <button className="btn btn-lg  outline-dashed   dark:hover:text-white">
               HIRE ME
             </button>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,43 +6,42 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   colors: {
-    'blue': '#1fb6ff',
-    'purple': '#7e5bef',
-    'pink': '#ff49db',
-    'orange': '#ff7849',
-    'green': '#13ce66',
-    'yellow': '#ffc82c',
-    'gray-dark': '#273444',
-    'gray': '#8492a6',
-    'gray-light': '#d3dce6',
+    blue: "#1fb6ff",
+    purple: "#7e5bef",
+    pink: "#ff49db",
+    orange: "#ff7849",
+    green: "#13ce66",
+    yellow: "#ffc82c",
+    "gray-dark": "#273444",
+    gray: "#8492a6",
+    "gray-light": "#d3dce6",
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   darkMode: "class",
 
   theme: {
     fontFamily: {
-      "Josefin": ["Josefin Sans", ...defaultTheme.fontFamily.sans],
-      "raleway": ["raleaway",...defaultTheme.fontFamily.sans],
-      "poppins": ["poppins ", ...defaultTheme.fontFamily.sans],
+      Josefin: ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+      raleway: ["raleaway", ...defaultTheme.fontFamily.sans],
+      poppins: ["poppins ", ...defaultTheme.fontFamily.sans],
     },
     screens: {
-      'sm': '0px',
+      sm: "0px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
 
     extend: {},
   },
-
-}
+};
